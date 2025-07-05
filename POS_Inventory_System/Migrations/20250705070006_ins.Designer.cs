c5f5731a6ec99163034aaceaeb6dbd7fb94d998b
AmanUllah <amanullah0884@gmail.com>
2025-07-05 13:01:48 +0600

    updt2

--- POS_Inventory_System/Migrations/20250705070006_ins.Designer.cs
+++ POS_Inventory_System/Migrations/20250705070006_ins.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using POS_Inventory_System.Models;
 
@@ -11,9 +12,11 @@ using POS_Inventory_System.Models;
 namespace POS_Inventory_System.Migrations
 {
     [DbContext(typeof(InventoryContext))]
-    partial class InventoryContextModelSnapshot : ModelSnapshot
+    [Migration("20250705070006_ins")]
+    partial class ins
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ POS_Inventory_System/Migrations/20250705070006_ins.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS_Inventory_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class ins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompanyInfos",
+                table: "CompanyInfos");
+
+            migrationBuilder.RenameTable(
+                name: "CompanyInfos",
+                newName: "CompanyInfo");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompanyInfo",
+                table: "CompanyInfo",
+                column: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompanyInfo",
+                table: "CompanyInfo");
+
+            migrationBuilder.RenameTable(
+                name: "CompanyInfo",
+                newName: "CompanyInfos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompanyInfos",
+                table: "CompanyInfos",
+                column: "ID");
+        }
+    }
+}
