3930371a70cc20b63242a25d49cf131c1de325da
AMAN ULLAH <amanullah0884@gmail.com>
2025-07-24 12:21:16 +0600

    up

---
+++ POS_System/Controllers/PurchaseMastersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using POS_Inventory_System.Models;
+using POS_Inventory_System.Repositories.Base;
+
+namespace POS_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseMastersController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PurchaseMastersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<List<PurchaseMasters>> Get()
+        {
+            var data = await _unitOfWork.PurchaseMastersRepo.GetAll(null, null);
+            return data.ToList();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult>Get(int id)
+        {
+            var find = await _unitOfWork.PurchaseMastersRepo.GetById(id);
+            if (find == null)
+            {
+                return NotFound();
+            }
+            return Ok(find);
+        }
+        //[HttpPost]
+        //public async Task<IEnumerable<>>
+     }
+}
+
